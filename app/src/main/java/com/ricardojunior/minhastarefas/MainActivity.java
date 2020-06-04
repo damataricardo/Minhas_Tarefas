b43270181c06687a1c6669d72fa5564c6b2cab16
Ricardo <ricardo-mata-junior@hotmail.com>
2020-06-04 10:56:14 -0300

    Initial commit

---
+++ app/src/main/java/com/ricardojunior/minhastarefas/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ricardojunior.minhastarefas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void entrar(View view) {
+        EditText login = (EditText) findViewById(R.id.login);
+        EditText senha = (EditText) findViewById(R.id.senha);
+
+        if ("admin".equals(login.getText().toString()) &&
+                "123".equals(senha.getText().toString())) {
+                    startActivity(new Intent(this, TarefaActivity.class));
+        }else {
+            Toast.makeText(this, "Login ou senha inválida!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
